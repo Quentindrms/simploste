36ce44e1bcd0e56e0e6bde832bba8fa6059a81bb
Quentin Derimais <quentin.derimais@gmail.com>
2025-07-18 11:38:53 +0200

    Co-authored-by: Estelle Haubois <haubois.estelle@gmail.com> Co-authored-by: Laurence-LaurencE <Laurence-LaurencE@users.noreply.github.com>

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,28 @@
 "use strict";
-const form = document.getElementById('form-title');
+const form = document.getElementById('form');
 const btnSubmit = document.getElementById('btnSubmit');
+const formData = new FormData(form);
+/** Objet dataUser - données liées au client  */
+let dataUser = {
+    nom: formData.get('nom'),
+    prenom: formData.get('prenom'),
+    dateBirth: formData.get('age'),
+    mail: formData.get('email'),
+    phone: formData.get('tel'),
+};
+/** Objet dataJourney - données liées au voyage du client */
+let dataJourney = {
+    arrival: formData.get('villeArrivee'),
+    dateTimeLocal: formData.get('depart')
+};
 if (btnSubmit) {
-    btnSubmit.addEventListener('submit', () => { submitForm(); });
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        submitForm();
+    });
 }
+console.log("hello world");
 function submitForm() {
-    console.log('Click');
+    console.log(dataUser);
+    console.log(dataJourney);
 }
