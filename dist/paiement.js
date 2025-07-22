bcb0edd75a7eb91d8f486a4e86ee12e036ce6cc7
Laurence-LaurencE <laurence20gros@gmail.com>
2025-07-22 22:53:11 +0200

    commit laurence_debut du code ds paiement.ts

--- dist/paiement.js
+++ dist/paiement.js
@@ -1,1 +1,26 @@
 "use strict";
+const paiementForm = document.getElementById("paiementform");
+paiementForm.addEventListener("submit", (event) => {
+    event === null || event === void 0 ? void 0 : event.preventDefault();
+    const formData = new FormData(paiementForm);
+    const paiementDatas = {
+        cb_name: formData.get("cb_name"),
+        cb_types: formData.get("cb_types"),
+        cb_number: formData.get("cb_number"),
+        cb_expiration: formData.get("cb_expiration"),
+        cb_security: formData.get("cb_security")
+    };
+    // console.log(paiementDatas);
+    const errors = [];
+    if (typeof paiementDatas.cb_name == "string") { /* && paiementDatas.cb_name.length <= 3*/
+        const namecb = paiementDatas.cb_name.trim();
+        if (namecb.length < 3) {
+            errors.push("le nom sur la carte deit contenir au moins 3 caratères.");
+        }
+        if (namecb !== namecb.toUpperCase()) {
+            errors.push("Le nom sur la carte doit être en majuscules.");
+        }
+        console.log(errors);
+        // !!!! ds HTML ajouter les condition cb_name en UpperCase !!!!! 
+    }
+});
