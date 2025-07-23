63e53d6f98d033ef006143fe23c615fe55e3346b
Laurence-LaurencE <laurence20gros@gmail.com>
2025-07-24 01:24:46 +0200

    commit laurence _paiement.js+.html : ajout messages errors

--- dist/paiement.js
+++ dist/paiement.js
@@ -14,33 +14,50 @@ if (paiementForm) {
         // console.log(paiementDatas);
         //____errors____________________
         const errors = [];
-        //____errors_cb_name_______________
+        //ok____errors_cb_name_______________ts+html
         if (typeof paiementDatas.cb_name == "string") { /* && paiementDatas.cb_name.length <= 3*/
-            const namecb = paiementDatas.cb_name.trim();
+            const namecb = paiementDatas.cb_name.trim(); //méthode .trim() pour qu'il n'y ait pas d'espace avant/apres le text saisi
             if (namecb.length < 3) {
                 errors.push("le nom sur la carte doit contenir au moins 3 caratères.");
             }
             if (namecb !== namecb.toUpperCase()) {
                 errors.push("Le nom sur la carte doit être en majuscules.");
             }
-            //console.log(errors);
-            //____errors_type carte_______________
-            if (!paiementDatas.cb_types) {
-                errors.push("Vous devez sélectionner un type de paiement");
-            }
-            //____errors_cb_number_______________
-            if (!paiementDatas.cb_types) {
-                errors.push("Vous devez renseigner le numero de votre carte");
-            }
-            //____errors_cb_expiration_______________
-            if (!paiementDatas.cb_expiration) {
-                errors.push("Vous devez renseigner la date d'expiration de votre carte");
-            }
-            if (typeof paiementDatas.cb_expiration == "string") { }
-            //____errors_ccv_______________
-            console.log(errors);
-            // !!!! ds HTML ajouter les condition cb_name en UpperCase !!!!! 
         }
+        //console.log(errors);
+        //ok____errors_type carte_______________
+        if (!paiementDatas.cb_types) {
+            errors.push("Vous devez sélectionner un type de paiement");
+        }
+        //console.log(errors);
+        //____errors_cb_number_______________algo de Lhun
+        if (!paiementDatas.cb_types) {
+            errors.push("Vous devez renseigner le numero de votre carte");
+        } //la fonction pour algo de Lhun est plus bas
+        //?____errors_cb_expiration_______________ts+html
+        if (!paiementDatas.cb_expiration) {
+            errors.push("Vous devez renseigner la date d'expiration de votre carte");
+        }
+        if (typeof paiementDatas.cb_expiration !== "string") {
+            errors.push("Vous devez renseigner le mois et l'année d'expiration de votre carte : MM/AAAA");
+        } // Attention : le text affiché ds le nav google n'est pas adapté au format firefox (trop ancien)=> quoi faire ?  => mettre une indication (ex: "ouvrez votre fomruaire dans google chrome" ?)
+        //console.log(errors);
+        //ok____errors_ccv_cb_security______________ ts+HTML
+        if (typeof paiementDatas.cb_security == "string") {
+            const cb_security = paiementDatas.cb_security.trim();
+            const isValidSecurityCode = /^[0-9]{3}$/.test(cb_security); //RegExp champs = 3 chiffres exactement
+            if (!isValidSecurityCode) {
+                errors.push("Vous devez saisir un code de sécurité de 3 chiffres");
+            }
+            // if (cb_security.length <3) {
+            //     errors.push ("Vous devez saisir un code de sécurité de 3 chiffres")
+            // }
+            // if (cb_security.length >3) {
+            //     errors.push ("Vous devez saisir un code de sécurité de 3 chiffres")
+            // }
+        }
+        console.log("paiement soumis : ", paiementDatas);
+        alert("paiement soumis avec succès");
     });
 }
 //____algo de Luhn__________
