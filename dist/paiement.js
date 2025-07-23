3217d65c3ecf269e4b8b5d49b77ed3da06c6fda5
Laurence-LaurencE <laurence20gros@gmail.com>
2025-07-23 16:56:20 +0200

    commit laurence _ modif html: name UpperCase +  js:suite des errors

--- dist/paiement.js
+++ dist/paiement.js
@@ -1,26 +1,50 @@
 "use strict";
 const paiementForm = document.getElementById("paiementform");
-paiementForm.addEventListener("submit", (event) => {
-    event === null || event === void 0 ? void 0 : event.preventDefault();
-    const formData = new FormData(paiementForm);
-    const paiementDatas = {
-        cb_name: formData.get("cb_name"),
-        cb_types: formData.get("cb_types"),
-        cb_number: formData.get("cb_number"),
-        cb_expiration: formData.get("cb_expiration"),
-        cb_security: formData.get("cb_security")
-    };
-    // console.log(paiementDatas);
-    const errors = [];
-    if (typeof paiementDatas.cb_name == "string") { /* && paiementDatas.cb_name.length <= 3*/
-        const namecb = paiementDatas.cb_name.trim();
-        if (namecb.length < 3) {
-            errors.push("le nom sur la carte deit contenir au moins 3 caratères.");
+if (paiementForm) {
+    paiementForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formData = new FormData(paiementForm);
+        const paiementDatas = {
+            cb_name: formData.get("cb_name"),
+            cb_types: formData.get("cb_types"),
+            cb_number: formData.get("cb_number"),
+            cb_expiration: formData.get("cb_expiration"),
+            cb_security: formData.get("cb_security")
+        };
+        // console.log(paiementDatas);
+        //____errors____________________
+        const errors = [];
+        //____errors_cb_name_______________
+        if (typeof paiementDatas.cb_name == "string") { /* && paiementDatas.cb_name.length <= 3*/
+            const namecb = paiementDatas.cb_name.trim();
+            if (namecb.length < 3) {
+                errors.push("le nom sur la carte doit contenir au moins 3 caratères.");
+            }
+            if (namecb !== namecb.toUpperCase()) {
+                errors.push("Le nom sur la carte doit être en majuscules.");
+            }
+            //console.log(errors);
+            //____errors_type carte_______________
+            if (!paiementDatas.cb_types) {
+                errors.push("Vous devez sélectionner un type de paiement");
+            }
+            //____errors_cb_number_______________
+            if (!paiementDatas.cb_types) {
+                errors.push("Vous devez renseigner le numero de votre carte");
+            }
+            //____errors_cb_expiration_______________
+            if (!paiementDatas.cb_expiration) {
+                errors.push("Vous devez renseigner la date d'expiration de votre carte");
+            }
+            if (typeof paiementDatas.cb_expiration == "string") { }
+            //____errors_ccv_______________
+            console.log(errors);
+            // !!!! ds HTML ajouter les condition cb_name en UpperCase !!!!! 
         }
-        if (namecb !== namecb.toUpperCase()) {
-            errors.push("Le nom sur la carte doit être en majuscules.");
-        }
-        console.log(errors);
-        // !!!! ds HTML ajouter les condition cb_name en UpperCase !!!!! 
-    }
-});
+    });
+}
+//____algo de Luhn__________
+function X(cb_number) {
+    //ici intégrer algo de Lhun
+    return true;
+}
--- src/paiement.ts
+++ src/paiement.ts
@@ -1,6 +1,8 @@
 const paiementForm = document.getElementById ("paiementform") as HTMLFormElement;
+if(paiementForm){
 paiementForm.addEventListener("submit", (event) => {
-    event?.preventDefault();
+    event.preventDefault();
+
 
     const formData = new FormData(paiementForm);
 
@@ -14,18 +16,46 @@ paiementForm.addEventListener("submit", (event) => {
 
     // console.log(paiementDatas);
     
+
+    //____errors____________________
     const errors: string[]= []
+
+    //____errors_cb_name_______________
     if (typeof paiementDatas.cb_name == "string" ) {  /* && paiementDatas.cb_name.length <= 3*/ 
         const namecb = paiementDatas.cb_name.trim();
 
     if (namecb.length <3) {
-        errors.push("le nom sur la carte deit contenir au moins 3 caratères.");
+        errors.push("le nom sur la carte doit contenir au moins 3 caratères.");
         }
     
     if (namecb !== namecb.toUpperCase()) {
         errors.push("Le nom sur la carte doit être en majuscules.")
     }
-    
+    //console.log(errors);
+        
+//____errors_type carte_______________
+
+    if (!paiementDatas.cb_types) {
+        errors.push ("Vous devez sélectionner un type de paiement")
+    }
+
+   
+//____errors_cb_number_______________
+
+if (!paiementDatas.cb_types) {
+        errors.push ("Vous devez renseigner le numero de votre carte")
+    }
+
+
+//____errors_cb_expiration_______________
+if(!paiementDatas.cb_expiration){
+    errors.push ("Vous devez renseigner la date d'expiration de votre carte")
+}
+
+if(typeof paiementDatas.cb_expiration == "string" ){}
+
+//____errors_ccv_______________
+
     console.log(errors);
     // !!!! ds HTML ajouter les condition cb_name en UpperCase !!!!! 
 
@@ -33,3 +63,14 @@ paiementForm.addEventListener("submit", (event) => {
 
 
 });
+}
+
+
+
+//____algo de Luhn__________
+function X (cb_number) {
+    //ici intégrer algo de Lhun
+    return true
+}
+
+
