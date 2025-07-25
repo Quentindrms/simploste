ae8818ed045e5ff41bb713350140532c79842819
Quentin Derimais <quentin.derimais@gmail.com>
2025-07-25 18:14:09 +0200

    Ajouts du README

--- dist/Voyage.js
+++ dist/Voyage.js
@@ -1,4 +1,4 @@
-import "./data.js";
+import { data } from "./data.js";
 export class Voyage {
     constructor(date, destination, classe) {
         this.date = date;
@@ -7,64 +7,25 @@ export class Voyage {
     }
     // METHODE DE CALCUL DU PRIX:
     calculPrix() {
-        const classeChoisie = document.getElementById('classe-voyage');
-        let prixAuKm = -1;
-        if (classeChoisie.value === "economy") {
-            prixAuKm = 0.1;
+        let prixAuKm = 0;
+        let trajet = 0;
+        const classeChoisie = document.getElementById("classe-voyage");
+        const choixDeClasse = classeChoisie.value;
+        for (let i = 0; i < data.standing.length; i++) {
+            if (choixDeClasse === data.standing[i].value) {
+                prixAuKm = data.standing[i].pricePerKm;
+                break;
+            }
         }
-        ;
-        if (classeChoisie.value === "business") {
-            prixAuKm = 0.25;
+        const villeChoisie = document.getElementById("villeArrivee");
+        let arrivee = villeChoisie.value;
+        for (let i = 0; i < data.destinations.length; i++) {
+            if (arrivee === data.destinations[i].value) {
+                trajet = data.destinations[i].distanceFromParis;
+                break;
+            }
         }
-        ;
-        if (classeChoisie.value === "first") {
-            prixAuKm = 0.1;
-        }
-        ;
-        const villeChoisie = document.getElementById('villeArrivee');
-        const ville = villeChoisie.value;
-        let trajet = -1;
-        if (ville === "paris") {
-            trajet = 0;
-        }
-        ;
-        if (ville === "london") {
-            trajet = 340;
-        }
-        ;
-        if (ville === "berlin") {
-            trajet = 1050;
-        }
-        ;
-        if (ville === "madrid") {
-            trajet = 1250;
-        }
-        ;
-        if (ville === "rome") {
-            trajet = 1100;
-        }
-        ;
-        if (ville === "lisbon") {
-            trajet = 1450;
-        }
-        ;
-        if (ville === "vienna") {
-            trajet = 1230;
-        }
-        ;
-        if (ville === "amsterdam") {
-            trajet = 510;
-        }
-        ;
-        if (ville === "brussels") {
-            trajet = 300;
-        }
-        ;
-        if (ville === "copenhagen") {
-            trajet = 1380;
-        }
-        ;
-        if (trajet === -1 || prixAuKm === -1) {
+        if (trajet === 0 || prixAuKm === 0) {
             throw new Error("trajet ou prixAuKm non défini");
         }
         const prixTotal = trajet * prixAuKm;
